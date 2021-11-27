114043403e45bb2819b97910c5ed13c60a888955
ailen-pasqualino <54822063+ailen-pasqualino@users.noreply.github.com>
2021-11-27 15:55:56 -0300

    Add files via upload

--- dist/js/main.js
+++ dist/js/main.js
@@ -9,18 +9,46 @@ console.log(calendarMonth);
 
 calendarMonth.click(function(){
     $(this).addClass("active");
+    $(this).attr("aria-pressed","true");
+
     calendarWeek.removeClass("active");
+    calendarWeek.removeAttr("aria-pressed");
+
     $("#calendar-month").removeClass("d-none");
     $("#calendar-month").addClass("d-block");
+    $("#calendar-month").removeAttr("aria-hidden");
+
+    $("#calendar-week").attr("aria-hidden","true");
     $("#calendar-week").addClass("d-none");
-    $("#desafios").addClass("d-none");
+
+    $("#challenges, #emotions-log").addClass("d-none");
 });
 
 calendarWeek.click(function(){
     $(this).addClass("active");
+    $(this).attr("aria-pressed","true");
+
     calendarMonth.removeClass("active");
+    calendarMonth.removeAttr("aria-pressed");
+
+    $("#calendar-month").attr("aria-hidden","true");
     $("#calendar-month").addClass("d-none");
+
     $("#calendar-week").removeClass("d-none");
-    $("#desafios").removeClass("d-none");
-    $("#desafios").addClass("d-block");
+    $("#calendar-week").removeAttr("aria-hidden");
+    
+    $("#challenges, #emotions-log").removeClass("d-none");
+    $("#challenges, #emotions-log").addClass("d-block");
+});
+
+//PASO RMEINDER
+$("#yes").click(function(){
+    $("#reminder-time").removeClass("d-none");
+    $("#reminder-time").addClass("d-block");
+    $("#reminderTime").focus();
+});
+
+$("#no").click(function(){
+    $("#reminder-time").removeClass("d-block");
+    $("#reminder-time").addClass("d-none");
 });
